06319c6bf919e00f2a806461a6d44dd61fbd5b4f
ethankelly <72452934+ethankelly@users.noreply.github.com>
2021-02-11 18:13:31 +0000

    Naive solution (without recursion yet)

--- src/io/github/ethankelly/NecessaryEquations.java
+++ src/io/github/ethankelly/NecessaryEquations.java
@@ -1,27 +1,8 @@
 package io.github.ethankelly;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class NecessaryEquations {
-
-    public int findNumberNeeded(Graph g, char[] states, int r) {
-        int numberNeeded = 0;
-
-        for (int i = 0; i < g.getNumVertices(); i++) {
-            for (int j = 0; j < g.getNumVertices(); j++) {
-                if (g.isEdge(i, j)) {
-
-                }
-            }
-        }
-        // Calculate the value of nCr
-        int n = states.length * g.getNumVertices();
-
-        return numberNeeded;
-    }
 
     public List<Vertex> findSingles(char[] states, int[] vertices) {
         List<Vertex> verticesWeNeed = new ArrayList<>();
@@ -42,13 +23,11 @@ public class NecessaryEquations {
                 toAdd.add(w);
                 Collections.sort(toAdd);
 
-                List<Vertex> toCheck = toAdd;
-                toCheck.sort(Collections.reverseOrder());
-
-                if (v.getState() != w.getState()
-                        && v.getLocation() != w.getLocation()
-                        && !(pairs.contains(toAdd) || pairs.contains(toCheck))
-                        && g.isEdge(v.getLocation(), w.getLocation())) {
+                if (Vertex.areStatesDifferent(toAdd)
+                        && Vertex.areLocationsDifferent(toAdd)
+                        && Vertex.areAllConnected(toAdd, g)
+                        && !(pairs.contains(toAdd))
+                ) {
                     pairs.add(toAdd);
                 }
             }
@@ -56,11 +35,64 @@ public class NecessaryEquations {
         return pairs;
     }
 
+    public List<List<Vertex>> findTriples(List<Vertex> singles, Graph g) {
+        List<List<Vertex>> triples = new ArrayList<>();
+        for (Vertex v : singles) {
+            for (Vertex w : singles) {
+                for (Vertex x : singles) {
+                    List<Vertex> toAdd = new ArrayList<>();
+                    toAdd.add(v);
+                    toAdd.add(w);
+                    toAdd.add(x);
+                    Collections.sort(toAdd);
+
+                    if (Vertex.areStatesDifferent(toAdd)
+                            && Vertex.areLocationsDifferent(toAdd)
+                            && Vertex.areAllConnected(toAdd, g)
+                            && !(triples.contains(toAdd)))
+                        triples.add(toAdd);
+                }
+            }
+        }
+        return triples;
+    }
+
+    public List<List<Vertex>> findTuples(Graph g, char[] states, int[] vertices) {
+
+        int tupleNumber = vertices.length;
+        List<Vertex> singles = this.findSingles(states, vertices);
+        List<List<Vertex>> tuples = new ArrayList<>();
+
+
+        while (tupleNumber > 0) {
+            switch (tupleNumber) {
+                case 1 -> {
+                    tuples.add(singles);
+                    tupleNumber--;
+                }
+                case 2 -> {
+                    tuples.addAll((this.findPairs(singles, g)));
+                    tupleNumber--;
+                }
+                case 3 -> {
+                    tuples.addAll(this.findTriples(singles, g));
+                    tupleNumber--;
+                }
+            }
+        }
+
+        return tuples;
+    }
+
+
+
     public static void main(String[] args) {
         NecessaryEquations ne = new NecessaryEquations();
 
-        char[] states = new char[]{'S','I'};
+        char[] states = new char[]{'S', 'I'};
 
+        System.out.println();
+        // TRIANGLE
         Graph triangle = new Graph(3);
         triangle.addEdge(0, 1);
         triangle.addEdge(1, 2);
@@ -68,43 +100,61 @@ public class NecessaryEquations {
         int[] triangleVertices = new int[triangle.getNumVertices()];
         Arrays.setAll(triangleVertices, i -> i);
 
-        Graph lollipop = new Graph(4);
-        lollipop.addEdge(0,1);
-        lollipop.addEdge(0,2);
-        lollipop.addEdge(0, 3);
-        lollipop.addEdge(2,3);
-        int[] lollipopVertices = new int[lollipop.getNumVertices()];
-        Arrays.setAll(lollipopVertices, i -> i);
-
-        Graph toast = new Graph(4);
-        toast.addEdge(0, 1);
-        toast.addEdge(0, 2);
-        toast.addEdge(0, 3);
-        toast.addEdge(1, 2);
-        toast.addEdge(2, 3);
-        int[] toastVertices = new int[toast.getNumVertices()];
-        Arrays.setAll(toastVertices, i -> i);
-
         System.out.println("Triangle graph:\n" + triangle);
         List<Vertex> triangleSingles = ne.findSingles(states, triangleVertices);
         List<List<Vertex>> trianglePairs = ne.findPairs(triangleSingles, triangle);
         triangleSingles.forEach(System.out::println);
         trianglePairs.forEach(System.out::println);
+        List<List<Vertex>> triangleTriples = ne.findTriples(triangleSingles, triangle);
+        triangleTriples.forEach(System.out::println);
         System.out.println();
 
-        System.out.println("Lollipop graph:\n" + lollipop);
-        List<Vertex> lollipopSingles = ne.findSingles(states, lollipopVertices);
-        List<List<Vertex>> lollipopPairs = ne.findPairs(lollipopSingles, lollipop);
-        lollipopSingles.forEach(System.out::println);
-        lollipopPairs.forEach(System.out::println);
-        System.out.println();
+//        // LOLLIPOP
+//        Graph lollipop = new Graph(4);
+//        lollipop.addEdge(0, 1);
+//        lollipop.addEdge(0, 2);
+//        lollipop.addEdge(0, 3);
+//        lollipop.addEdge(2, 3);
+//        int[] lollipopVertices = new int[lollipop.getNumVertices()];
+//        Arrays.setAll(lollipopVertices, i -> i);
+//
+//        System.out.println("Lollipop graph:\n" + lollipop);
+//        List<Vertex> lollipopSingles = ne.findSingles(states, lollipopVertices);
+//        List<List<Vertex>> lollipopPairs = ne.findPairs(lollipopSingles, lollipop);
+//        lollipopSingles.forEach(System.out::println);
+//        lollipopPairs.forEach(System.out::println);
+//        List<List<Vertex>> lollipopTriples = ne.findTriples(lollipopSingles, lollipop);
+//        lollipopTriples.forEach(System.out::println);
+//        System.out.println();
+//
+//        // TOAST
+//        Graph toast = new Graph(4);
+//        toast.addEdge(0, 1);
+//        toast.addEdge(0, 2);
+//        toast.addEdge(0, 3);
+//        toast.addEdge(1, 2);
+//        toast.addEdge(2, 3);
+//        int[] toastVertices = new int[toast.getNumVertices()];
+//        Arrays.setAll(toastVertices, i -> i);
+//
+//        System.out.println("Toast graph:\n" + toast);
+//        List<Vertex> toastSingles = ne.findSingles(states, toastVertices);
+//        toastSingles.forEach(System.out::println);
+//        List<List<Vertex>> toastPairs = ne.findPairs(toastSingles, toast);
+//        toastPairs.forEach(System.out::println);
+//        List<List<Vertex>> toastTriples = ne.findTriples(toastSingles, toast);
+//        toastTriples.forEach(System.out::println);
+//        System.out.println();
 
-        System.out.println("Toast graph:\n" + toast);
-        List<Vertex> toastSingles = ne.findSingles(states, toastVertices);
-        toastSingles.forEach(System.out::println);
-        List<List<Vertex>> toastPairs = ne.findPairs(toastSingles, toast);
-        toastPairs.forEach(System.out::println);
-        System.out.println();
+
+        System.out.println("Testing tuple method on triangle...");
+        List<List<Vertex>> tuples = ne.findTuples(triangle, states, triangleVertices);
+        for (List<Vertex> tuple : tuples) {
+
+                System.out.println(tuple);
+
+        }
+
 
     }
 }
--- src/io/github/ethankelly/Vertex.java
+++ src/io/github/ethankelly/Vertex.java
@@ -1,6 +1,7 @@
 package io.github.ethankelly;
 
-import java.util.Comparator;
+import java.util.List;
+import java.util.stream.IntStream;
 
 public class Vertex implements Comparable<Vertex> {
     private final char state;
@@ -19,12 +20,45 @@ public class Vertex implements Comparable<Vertex> {
         return location;
     }
 
+    public static boolean areStatesDifferent(List<Vertex> toCheck) {
+        boolean statesDifferent = false;
+
+        for (Vertex v : toCheck) {
+            for (Vertex w : toCheck) {
+                if (v.getState() != w.getState()) {
+                    statesDifferent = true;
+                    break;
+                }
+            }
+        }
+        return statesDifferent;
+    }
+
+    public static boolean areLocationsDifferent(List<Vertex> toCheck) {
+        boolean locationsDifferent = true;
+        for (Vertex v : toCheck) {
+            if (toCheck.stream().anyMatch(w -> v.getLocation() == w.getLocation() && v != w)) {
+                locationsDifferent = false;
+            }
+        }
+        return locationsDifferent;
+    }
+
+    public static boolean areAllConnected(List<Vertex> toCheck, Graph g) {
+        return IntStream.range(0, toCheck.size() - 1).allMatch(
+                i -> g.isEdge(toCheck.get(i).getLocation(),
+                        toCheck.get(i + 1).getLocation())
+        );
+    }
+
+
     @Override
     public String toString() {
         return "\u3008" + this.getState() + this.getLocation() + "\u3009";
     }
 
-    @Override public int compareTo(Vertex that) {
+    @Override
+    public int compareTo(Vertex that) {
         final int BEFORE = -1;
         final int EQUAL = 0;
         final int AFTER = 1;
@@ -44,9 +78,8 @@ public class Vertex implements Comparable<Vertex> {
         return EQUAL;
     }
 
-
     public static void main(String[] args) {
-        Vertex test = new Vertex('S',1);
+        Vertex test = new Vertex('S', 1);
         System.out.println(test);
     }
 }
