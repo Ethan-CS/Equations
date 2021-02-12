c3b3eff558de96b9a0ac94df60fb4fb220b28939
ethankelly <72452934+ethankelly@users.noreply.github.com>
2021-02-12 00:30:26 +0000

    Completed methods. Correct result obtained on three graphs tested, also added Javadocs

---
+++ src/io/github/ethankelly/Equations.java
@@ -0,0 +1,199 @@
+package io.github.ethankelly;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * The {@code Equations} class is used to determine and represent the number of differential equations required to fully
+ * describe a given compartmental epidemiological model on a particular graph. The class contains methods that determine
+ * the total number of equations that would be required to exactly describe the dynamics of a compartmental model with
+ * some number of states (for instance, susceptible/infected/recovered - SIR) and a given graph (with a number of
+ * vertices connected in some way by a number of edges). The graphs may be randomly generated or hard-coded and the
+ * states can be specified when running the model.
+ */
+public class Equations {
+    private final Graph graph;
+    private final char[] states;
+
+    /**
+     * Class constructor - assigns a graph and some array of states to a new Equations object.
+     *
+     * @param graph the graph of which we are interested in equations.
+     * @param states the states that a vertex can be in (e.g. SIP).
+     */
+    public Equations(Graph graph, char[] states) {
+        this.graph = graph;
+        this.states = states;
+    }
+
+    /**
+     * Given the equations necessary to exactly describe an SIR epidemic on some graph, returns the numbers of n-tuple
+     * equations that are required.
+     *
+     * @param equations the equations to find the numbers of each length.
+     * @return An array with the number of equations of each size required to exactly describe the SIR system.
+     */
+    public int[] findNumbers(List<List<Vertex>> equations) {
+        // The array needs as many elements as there are different sizes in the equations list,
+        // So initialise to (1 less than) the size of the largest (final) sub-list.
+        int[] sizes = new int[equations.get(equations.size() - 1).size()];
+        // For each sub-list we come across, increase the element at the array position corresponding
+        // to its size.
+        equations.forEach(equation -> sizes[equation.size() - 1]++);
+
+        return sizes;
+    }
+
+    /**
+     * Using the states required in the model and the number of vertices in the graph, this method determines the full
+     * list of single termed equations that are involved in describing the model exactly.
+     *
+     * @return the number of single-probability differential equations required by the model.
+     */
+    public List<Vertex> findSingles() {
+        // Get states, graph and the vertices in the associated graph for the model
+        char[] states = this.getStates();
+        Graph graph = this.getGraph();
+        int numVertices = graph.getNumVertices();
+        int[] vertices = new int[numVertices];
+        Arrays.setAll(vertices, i -> i);
+
+        // We need an equation for the probability of each vertex being in each state
+        List<Vertex> verticesWeNeed = new ArrayList<>();
+        for (char c : states) {
+            for (int v : vertices) {
+                verticesWeNeed.add(new Vertex(c, v));
+            }
+        }
+        return verticesWeNeed;
+    }
+
+    // Recursive helper method to generate the total equations we need
+    private void generateTuples(List<Vertex> items,
+                                List<Vertex> selected,
+                                int index,
+                                List<List<Vertex>> result) {
+        if (index >= items.size()) {
+            result.add(new ArrayList<>(selected));
+        } else {
+            generateTuples(items, selected, index + 1, result);
+            selected.add(items.get(index));
+            generateTuples(items, selected, index + 1, result);
+            selected.remove(selected.size() - 1);
+        }
+    }
+
+    /**
+     * Using the graph we are determining equations for and the states that a vertex can take, this method finds all of
+     * the equations that we need to use to exactly express the system dynamics of the model in question.
+     *
+     * @return a list of each n-tuple we are required to express to exactly detail the model system dynamics.
+     */
+    public List<List<Vertex>> generateTuples() {
+        List<Vertex> items = this.findSingles();
+        List<List<Vertex>> result = new ArrayList<>();
+        List<Vertex> selected = new ArrayList<>();
+        generateTuples(items, selected, 0, result);
+
+        // The helper method gives us all combinations, so remove the ones that don't
+        // constitute valid (necessary) tuples (and sort by length of sub-arrays).
+        result.removeIf(row -> !this.isValidTuple(row));
+        result.sort(Comparator.comparingInt(List::size));
+
+        return result;
+    }
+
+    /**
+     * Given a potential tuple, this method checks that the states of each probability are not all the same, that each
+     * probability corresponds to a different vertex and that all of the vertices involved are in fact connected. If
+     * these three conditions are met, then it is a tuple that we are required to express in the total set of equations
+     * describing the system dynamics.
+     *
+     * @param toAdd the potential tuple we are considering adding to the system dynamics.
+     * @return true if the tuple is essential to expressing the system dynamics, false otherwise.
+     */
+    private boolean isValidTuple(List<Vertex> toAdd) {
+        Graph g = this.getGraph();
+        return Vertex.areStatesDifferent(toAdd)
+                && Vertex.areLocationsDifferent(toAdd)
+                && Vertex.areAllConnected(toAdd, g);
+    }
+
+    /**
+     * @return the graph associated with the model in question.
+     */
+    public Graph getGraph() {
+        return graph;
+    }
+
+    /**
+     * Each vertex in the graph model can be in any of the given states, which might be 'Susceptible,' 'Infected' and
+     * 'Recovered' (standard SIR model) for instance.
+     *
+     * @return the states defined for the model in question.
+     */
+    public char[] getStates() {
+        return states;
+    }
+
+    /**
+     * Unit testing for the {@code Equations} class and contained methods.
+     * @param args the command-line arguments.
+     */
+    public static void main(String[] args) {
+        char[] states = new char[]{'S', 'I'};
+        System.out.println();
+
+        // TRIANGLE
+        Graph triangle = new Graph(3);
+        triangle.addEdge(0, 1);
+        triangle.addEdge(1, 2);
+        triangle.addEdge(2, 0);
+        Equations triangleNE = new Equations(triangle, states);
+
+        System.out.println(" *** TRIANGLE GRAPH ***\n\n" + triangle);
+
+        System.out.println("Equations required for Triangle graph:");
+        List<List<Vertex>> triangleEquations = triangleNE.generateTuples();
+        triangleEquations.forEach(System.out::println);
+        System.out.println("\nNumbers of equations of each size: "
+                + Arrays.toString(triangleNE.findNumbers(triangleEquations)) + "\n\n");
+
+        // LOLLIPOP
+        Graph lollipop = new Graph(4);
+        lollipop.addEdge(0, 1);
+        lollipop.addEdge(0, 2);
+        lollipop.addEdge(0, 3);
+        lollipop.addEdge(2, 3);
+        Equations lollipopNE = new Equations(lollipop, states);
+
+        System.out.println(" *** LOLLIPOP GRAPH ***\n\n" + lollipop);
+
+        System.out.println("Equations required for Lollipop graph:");
+        List<List<Vertex>> lollipopEquations = lollipopNE.generateTuples();
+        lollipopEquations.forEach(System.out::println);
+        System.out.println("\nNumbers of equations of each size: "
+                + Arrays.toString(lollipopNE.findNumbers(lollipopEquations)) + "\n\n");
+
+        // TOAST
+        Graph toast = new Graph(4);
+        toast.addEdge(0, 1);
+        toast.addEdge(0, 2);
+        toast.addEdge(0, 3);
+        toast.addEdge(1, 2);
+        toast.addEdge(2, 3);
+        Equations toastNE = new Equations(toast, states);
+
+        System.out.println(" *** TOAST GRAPH ***\n\n" + toast);
+        System.out.println();
+
+        System.out.println("Equations required for Toast graph:\n");
+        List<List<Vertex>> toastEquations = toastNE.generateTuples();
+        toastEquations.forEach(System.out::println);
+        System.out.println("\nNumbers of equations of each size: "
+                + Arrays.toString(toastNE.findNumbers(toastEquations)) + "\n\n");
+    }
+
+}
--- src/io/github/ethankelly/Vertex.java
+++ src/io/github/ethankelly/Vertex.java
@@ -3,39 +3,79 @@ package io.github.ethankelly;
 import java.util.List;
 import java.util.stream.IntStream;
 
+/**
+ * The {@code Vertex} class represents an instance of a vertex being in a particular state in a compartmental
+ * epidemiological model. Each vertex object has a state (for instance, {@code 'S'} for "Susceptible") and a numerical
+ * location in the graph we are interested in.
+ */
 public class Vertex implements Comparable<Vertex> {
     private final char state;
     private final int location;
 
+    /**
+     * Class constructor, assigning a state and location to a vertex instance object.
+     *
+     * @param state the state for which we are interested in studying the probability of the vertex being in.
+     * @param location the numerical index that represents the location of the vertex in the associated graph.
+     */
     public Vertex(char state, int location) {
         this.state = state;
         this.location = location;
     }
 
+    /**
+     * @return the state of the vertex.
+     */
     public char getState() {
         return state;
     }
 
+    /**
+     * @return the index location of the vertex.
+     */
     public int getLocation() {
         return location;
     }
 
+    /**
+     * Given a list of vertices (some tuple), this method checks whether all of the states are the same. If they are the same, we do
+     * not have to consider the associated equation of the tuple in the final system of equations that describes our
+     * compartmental model.
+     *
+     * @param toCheck the tuple we wish to verify has at least two different states in its elements.
+     * @return true if at least one vertex state is different to that of the others, false if they are all the same.
+     */
     public static boolean areStatesDifferent(List<Vertex> toCheck) {
         boolean statesDifferent = false;
-
-        for (Vertex v : toCheck) {
-            for (Vertex w : toCheck) {
-                if (v.getState() != w.getState()) {
+        // If there's only one vertex in the list, by definition we require it
+        // in the system of equations so we ensure this method returns true.
+        if (toCheck.size() == 1) {
+            statesDifferent = true;
+        } else {
+            // We only need to find two different states to know that the states are
+            // at least not all the same, returning true.
+            for (Vertex v : toCheck) {
+                if (toCheck.stream().anyMatch(w -> v.getState() != w.getState())) {
                     statesDifferent = true;
-                    break;
                 }
             }
         }
         return statesDifferent;
     }
 
+    /**
+     * Given some list of vertices (some tuple), this method checks whether the index locations of every element of the
+     * tuple are all distinct. If even two of the vertices have the same location, it is not a valid tuple for our
+     * purposes and we will not hve to consider the associated equation in the final system of equations that describes
+     * our compartmental model.
+     *
+     * @param toCheck the tuple we wish to verify has no repeated vertex locations.
+     * @return true if no index location is repeated in the vertices, false otherwise.
+     */
     public static boolean areLocationsDifferent(List<Vertex> toCheck) {
         boolean locationsDifferent = true;
+        // We only need to find two vertices in the list that have the same index location
+        // to know we don't have a required tuple, returning false.
         for (Vertex v : toCheck) {
             if (toCheck.stream().anyMatch(w -> v.getLocation() == w.getLocation() && v != w)) {
                 locationsDifferent = false;
@@ -44,42 +84,66 @@ public class Vertex implements Comparable<Vertex> {
         return locationsDifferent;
     }
 
+    /**
+     * Given some list of vertices (some tuple), this method verifies that all of the vertices in the graph given are in
+     * fact connected. That is, we are only interested in a tuple of vertices that constitute some manner of path (cycle
+     * or such like) - if not, then we do not have to consider the associated equation in the final system of equations
+     * that describes our compartmental model and we can discount the given tuple/
+     *
+     * @param toCheck the tuple we wish to check constitutes some kind of path.
+     * @param g the graph in which the tuple exists.
+     * @return true if the tuple forms a path in some way, false otherwise.
+     */
     public static boolean areAllConnected(List<Vertex> toCheck, Graph g) {
-        return IntStream.range(0, toCheck.size() - 1).allMatch(
-                i -> g.isEdge(toCheck.get(i).getLocation(),
-                        toCheck.get(i + 1).getLocation())
-        );
+        // If there's only one vertex in the list, by definition we require it
+        // in the system of equations so we ensure this method returns true.
+        if (toCheck.size() == 1) {
+            return true;
+        } else {
+            // Return whether the vertices have some path between them
+            return IntStream.range(0, toCheck.size() - 1).allMatch(
+                    i -> g.isEdge(toCheck.get(i).getLocation(),
+                            toCheck.get(i + 1).getLocation())
+            );
+        }
     }
 
-
+    /**
+     * Represents a given vertex textually, for printing to standard output.
+     *
+     * @return a String representation of the vertex.
+     */
     @Override
     public String toString() {
+        // \u3008 - left angle bracket ($\langle$)
+        // \u3009 - right angle bracket ($\rangle$)
         return "\u3008" + this.getState() + this.getLocation() + "\u3009";
     }
 
+    /**
+     * Gives a method to compare two vertices. This comparison is done using the numerical index locations of the two
+     * vertices we wish to compare.
+     *
+     * @param that the vertex to which we wish to compare the vertex in question.
+     * @return -1 if the location of {@code that} is greater than the vertex in question, 0 if they are the same and +1
+     * if the location of {@code that} is less than the vertex in question.
+     */
     @Override
     public int compareTo(Vertex that) {
         final int BEFORE = -1;
         final int EQUAL = 0;
         final int AFTER = 1;
 
-        //this optimization is usually worthwhile, and can
-        //always be added
         if (this == that) return EQUAL;
 
-        //primitive numbers follow this form
+        // Primitive numbers follow this form
         if (this.getLocation() < that.getLocation()) return BEFORE;
         if (this.getLocation() > that.getLocation()) return AFTER;
 
-        //all comparisons have yielded equality
-        //verify that compareTo is consistent with equals (optional)
+        // All comparisons have yielded equality
+        // verify that compareTo is consistent with equals (optional)
         assert this.equals(that) : "compareTo inconsistent with equals.";
 
         return EQUAL;
-    }
-
-    public static void main(String[] args) {
-        Vertex test = new Vertex('S', 1);
-        System.out.println(test);
     }
 }
