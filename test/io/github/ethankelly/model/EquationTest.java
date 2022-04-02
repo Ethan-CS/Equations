f1ba4d7fd6288f9c417af6688b67204365270483
Ethan Kelly <e.kelly.1@research.gla.ac.uk>
2022-04-02 13:01:50 +0100

    Added susceptible singles lollipop tests

--- test/io/github/ethankelly/model/EquationTest.java
+++ test/io/github/ethankelly/model/EquationTest.java
@@ -41,6 +41,23 @@ class EquationTest {
     }
 
     @Test
+    void testLollipopS1() {
+        // 〈S1〉=-τ〈S1 I2〉-τ〈S1 I3〉-τ〈S1 I4〉
+        Tuple S1 = new Tuple(new Vertex('S', 0));
+        Equation eqnS1 = new Equation(S1, SIR, lollipop);
+
+        Map<Tuple, List<Double>> expectedTerms = new HashMap<>();
+        expectedTerms.put(new Tuple(List.of(new Vertex('S', 0), new Vertex('I', 1))),
+                new ArrayList<>(List.of(-0.6)));
+        expectedTerms.put(new Tuple(List.of(new Vertex('S', 0), new Vertex('I', 2))),
+                new ArrayList<>(List.of(-0.6)));
+        expectedTerms.put(new Tuple(List.of(new Vertex('S', 0), new Vertex('I', 3))),
+                new ArrayList<>(List.of(-0.6)));
+
+        Assertions.assertEquals(eqnS1.getTerms(), expectedTerms);
+    }
+
+    @Test
     void testLollipopI2() {
         // 〈I2〉=τ〈I1 S2〉-γ〈I2〉
         Tuple I2 = new Tuple(new Vertex('I', 1));
@@ -53,6 +70,19 @@ class EquationTest {
                 new ArrayList<>(List.of(-0.1)));
 
         Assertions.assertEquals(eqnI2.getTerms(), expectedTerms);
+    }
+
+    @Test
+    void testLollipopS2() {
+        // 〈S2〉=-τ〈I1 S2〉
+        Tuple S2 = new Tuple(new Vertex('S', 1));
+        Equation eqnS2 = new Equation(S2, SIR, lollipop);
+
+        Map<Tuple, List<Double>> expectedTerms = new HashMap<>();
+        expectedTerms.put(new Tuple(List.of(new Vertex('I', 0), new Vertex('S', 1))),
+                new ArrayList<>(List.of(-0.6)));
+
+        Assertions.assertEquals(eqnS2.getTerms(), expectedTerms);
     }
 
     @Test
@@ -73,6 +103,21 @@ class EquationTest {
     }
 
     @Test
+    void testLollipopS3() {
+        //〈S3〉=-τ〈I1 S3〉-τ〈S3 I4〉
+        Tuple S3 = new Tuple(new Vertex('S', 2));
+        Equation eqnS3 = new Equation(S3, SIR, lollipop);
+
+        Map<Tuple, List<Double>> expectedTerms = new HashMap<>();
+        expectedTerms.put(new Tuple(List.of(new Vertex('I', 0), new Vertex('S', 2))),
+                new ArrayList<>(List.of(-0.6)));
+        expectedTerms.put(new Tuple(List.of(new Vertex('S', 2), new Vertex('I', 3))),
+                new ArrayList<>(List.of(-0.6)));
+
+        Assertions.assertEquals(eqnS3.getTerms(), expectedTerms);
+    }
+
+    @Test
     void testLollipopI4() {
         //〈I4〉=τ〈I1 S4〉+τ〈I3 S4〉-γ〈I4〉
         Tuple I4 = new Tuple(new Vertex('I', 3));
@@ -87,5 +132,20 @@ class EquationTest {
                 new ArrayList<>(List.of(-0.1)));
 
         Assertions.assertEquals(eqnI4.getTerms(), expectedTerms);
+    }
+
+    @Test
+    void testLollipopS4() {
+        // 〈S4〉=-τ〈I1 S4〉-τ〈I3 S4〉
+        Tuple S4 = new Tuple(new Vertex('S', 3));
+        Equation eqnS4 = new Equation(S4, SIR, lollipop);
+
+        Map<Tuple, List<Double>> expectedTerms = new HashMap<>();
+        expectedTerms.put(new Tuple(List.of(new Vertex('I', 0), new Vertex('S', 3))),
+                new ArrayList<>(List.of(-0.6)));
+        expectedTerms.put(new Tuple(List.of(new Vertex('I', 2), new Vertex('S', 3))),
+                new ArrayList<>(List.of(-0.6)));
+
+        Assertions.assertEquals(eqnS4.getTerms(), expectedTerms);
     }
 }
